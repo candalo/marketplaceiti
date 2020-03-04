9e48cefdb58f9daf9f2ac4743c466d1b242f139d
Lucas Candalo <candalolucas@gmail.com>
2020-03-04 11:51:16 -0300

    First commit

---
+++ app/src/main/java/com/github/candalo/marketplaceiti/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.github.candalo.marketplaceiti
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+    }
+
+}
