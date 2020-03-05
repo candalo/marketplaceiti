91784722f526565cf68da8352f4c061b4834c76f
Lucas Candalo <candalolucas@gmail.com>
2020-03-05 17:10:25 -0300

    Add action to continue shopping button
    Signed-off-by: Lucas Candalo <candalolucas@gmail.com>

--- app/src/main/java/com/github/candalo/marketplaceiti/CartProductsFragment.kt
+++ app/src/main/java/com/github/candalo/marketplaceiti/CartProductsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import br.com.zup.beagle.utils.loadView
 import br.com.zup.beagle.view.ScreenRequest
 import kotlinx.android.synthetic.main.fragment_cart_products.*
@@ -20,6 +21,11 @@ class CartProductsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainContent.loadView(this, ScreenRequest("/cart/products"))
+        continueShopping.setOnClickListener { backToProductsScreen() }
     }
 
+    private fun backToProductsScreen() {
+        (context as MainActivity).supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        (context as MainActivity).openFragment(ProductsFragment())
+    }
 }
